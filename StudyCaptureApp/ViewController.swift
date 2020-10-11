23e61f8ecd7427d38ec68c1b6f7b4af64f1fca00
Dan Brown <danbrown080@hotmail.com>
2020-10-11 17:36:08 -0400

    Initial Commit

---
+++ StudyCaptureApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudyCaptureApp
+//
+//  Created by Dan Brown on 10/11/20.
+//  Copyright © 2020 DanielBrown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
